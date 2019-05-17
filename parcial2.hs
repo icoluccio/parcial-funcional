f7cc23240cf326fe5a34bccbc804ce79a4f8f368
lspigariol <lspigariol@gmail.com>
2019-05-17 18:10:21 -0300

    arreglos de enunciado y solucion

--- parcial2.hs
+++ parcial2.hs
@@ -1,15 +1,15 @@
 import Text.Show.Functions
-type Requisito = Depto -> Bool
+type Requisito = Propiedad  -> Bool
 type Busqueda = [Requisito]
-type Depto = (Int, Int, Int, String)
-type Persona = (String, [Busqueda])
-ambientes (a, _,_,_) =a
-superficie (_,m2,_,_) = m2
-precio (_,_,p,_)= p
-barrio (_,_,_,b) = b
+data Propiedad = Depto {
+ ambientes::Int, 
+ superficie::Int, 
+ precio::Float, 
+ barrio::String}
+data Usuario = Persona {
+ mail::String, 
+ busqueda::Busqueda }
 
-mail persona = fst persona
-busquedas persona = snd persona
 
 ordenarSegun :: (a -> a -> Bool) -> [a] -> [a]
 ordenarSegun _ [] = []
@@ -19,15 +19,12 @@ between x y z = x <= z && y >= z
 
 
 -- 1
-comparar funcionComparacion funcion a b = funcionComparacion (funcion a) (funcion b)
-mayor funcion a b = comparar (>) funcion a b
-menor funcion a b = comparar (<) funcion a b
+mayor funcion a b = funcion a > funcion b
 
 -- 1a
 -- ordenarSegun (criterio) lista, donde criterio espera 2 elementos y devuelve booleano
 -- mayor espera un criterio y 2 elementos -> aplicación parcial
 -- ordenarSegun (mayor id) [1,2,3]
--- ordenarSegun (menor (*5)) [1,2,3]
 
 -- 2
 ubicadoEn listaDeBarrios departamento = any (==(barrio departamento)) listaDeBarrios
@@ -41,12 +38,18 @@ buscar busqueda criterioDeOrdenamiento listaDeDepartamentos =
     ordenarSegun criterioDeOrdenamiento
                   (filter (cumpleBusqueda busqueda) listaDeDepartamentos)
 --3c
-departamentos = [(1,2,3, "Recoleta"), (1,267,3, "Recoleta"), (1,2000,33, "Recoleta"), (1,20,300000, "Recoleta"), (1,20,300000, "Chacarita")]
+propiedadesDeEjemplo::[Propiedad ]
+propiedadesDeEjemplo = [Depto 3 80 12500 "Palermo", Depto 1 45 10000 "Villa  Urquiza", Depto 2 50 16000 "Palermo", Depto 1 45 10500 "Recoleta"]
 -- buscar [ubicadoEn ["Recoleta", "Palermo"], (<3).ambientes, (<6000).precio ] (mayor superficie) departamentos
 
 -- 4
-mailsDePersonasInteresadas departamento listaDePersonas = (map mail) (filter (cumpleAlgunaBusquedaPara departamento) listaDePersonas)
-cumpleAlgunaBusquedaPara departamento persona = any (flip cumpleBusqueda departamento) persona
+mailsDePersonasInteresadas:: Propiedad->[Usuario]->[String]
+mailsDePersonasInteresadas departamento personas = (map mail.filter (flip cumpleBusqueda departamento.busqueda)) personas
+
+--cumpleAlgunaBusquedaPara::Propiedad ->Usuario->Bool
+--cumpleAlgunaBusquedaPara departamento persona =  cumpleBusqueda departamento) (busqueda persona)
 -- 4 prueba
-personas = [("hola@hola.com", [ubicadoEn ["Algun lado"]]), ("hola2@hola2.com", [ubicadoEn ["Recoleta", "Palermo"], (<3).ambientes, (<6000).precio ]), ("hola3@hola.com", [ubicadoEn ["Recoleta", "Palermo"], (<3).ambientes, (<6000).precio])]
+
+personasDeEjemplo::[Usuario]
+personasDeEjemplo = [Persona "hola@hola.com" [ubicadoEn ["Algun lado"]], Persona "hola2@hola2.com" [ubicadoEn ["Recoleta", "Palermo"], (<3).ambientes, (<6000).precio ], Persona "hola3@hola.com" [ubicadoEn ["Recoleta", "Palermo"], (<3).ambientes, (<6000).precio]]
 -- mailsDePersonasInteresadas (head departamentos) personas
